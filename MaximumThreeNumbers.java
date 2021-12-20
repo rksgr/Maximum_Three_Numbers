4ef72c01e9bc39e7d60f9a4f359bf0523c792b2b
rksgr <r_kjha@yahoo.com>
2021-12-20 09:47:35 +0530

    Refactored: Maximum of three floating point numbers

--- MaximumThreeNumbers.java
+++ MaximumThreeNumbers.java
@@ -3,7 +3,9 @@ package com.mycompany.dec_18_max_nos_generics;
 
 
 public class MaximumThreeNumbers {
-    // Find maximum of three integers
+    /*
+    Use Case 1: Find maximum of three integers
+    */
     public static int maxThreeNumbers(int a,int b,int c){
         int max;
         Integer num1 = (Integer) a;
@@ -20,9 +22,29 @@ public class MaximumThreeNumbers {
         System.out.println("The largest of three numbers is: "+max);
         return max;
     }
+    /*
+    Use Case 2: Find maximum of three Floating point numbers
+    */
+    public static double maxThreeNumbers(double a,double b,double c){
+        double max;
+        Double num1 = (Double) a;
+        Double num2 = (Double) b;
+        Double num3 = (Double) c;
+
+        if ((num1.compareTo(num2))>0 && (num1.compareTo(num3))>0){
+            max = a;
+        } else if (num2.compareTo(num3)>0){
+            max  = b;
+        }else{
+            max = c;
+        }
+        System.out.println("The largest of three float numbers is: " + max);
+        return max;
+    }
+        
     public static void main(String []args){
         System.out.println("Welcome to BridgeLabz!");
         System.out.println("Today we shall find maximum among three numbers using Generics.");
-        maxThreeNumbers(52,23,44);
+        maxThreeNumbers(5.2,23.5,4.6);
     }
 }
