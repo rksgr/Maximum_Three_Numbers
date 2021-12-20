46705e8876a62da78b6469ae9c4053a2b3bd9c8f
rksgr <r_kjha@yahoo.com>
2021-12-20 09:36:20 +0530

    Added: Welcome Message

---
+++ MaximumThreeNumbers.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.dec_18_max_nos_generics;
+
+
+public class MaximumThreeNumbers {
+    public static void main(String []args){
+        System.out.println("Welcome to BridgeLabz!");
+        System.out.println("Today we shall find maximum among three numbers using Generics.");
+    }
+}
